9090af7df599cb10314dec4722b9de5243eaa69d
Jaime Cabrera <cabrerajaimeandres@gmail.com>
2020-09-21 23:47:34 -0500

    Crear y Guardar Rol

--- src/main/java/com/reservas/controller/RolController.java
+++ src/main/java/com/reservas/controller/RolController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.reservas.entity.Rol;
@@ -16,14 +18,31 @@ import com.reservas.service.RolService;
 public class RolController {
 
 	@Autowired
-	RolService rol;
+	RolService rolService;
 
 	@GetMapping("/")
 	public String listaRoles(Model model) {
-		List<Rol> listaRoles = rol.listarRoles();
+		List<Rol> listaRoles = rolService.listarRoles();
 		model.addAttribute("titulo", "Lista de Roles");
 		model.addAttribute("listaRoles", listaRoles);
 		return "/views/roles/listar";
+	}
+
+	@GetMapping("/crear")
+	public String crear(Model model) {
+		Rol rol = new Rol();
+		model.addAttribute("titulo", "Crear Roles");
+		model.addAttribute("rol", rol);
+		return "views/roles/crear";
+	}
+
+	@PostMapping("/guardar")
+	public String guardar(@ModelAttribute("rol") Rol rol) {
+		if (rol != null) {
+			rolService.guardar(rol);
+			return "redirect:/views/roles/";
+		}
+		return "views/roles/";
 	}
 
 }
